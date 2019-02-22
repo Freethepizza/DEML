8b88cc77bde9db8c74775dec8b6d3fbb880fc81a
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-02-22 13:31:11 +0100

    Added shit
    Stacker works boi

---
+++ new.py
@@ -0,0 +1,45 @@
+#
+#    .___             .__
+#  __| _/____   _____ |  |
+# / __ |/ __ \ /     \|  |
+#/ /_/ \  ___/|  Y Y  \  |__
+#\____ |\___  >__|_|  /____/
+#     \/    \/      \/
+#
+#Don't expect a markup language (En serio, no lo es.)
+open = open("test.deml", "r")
+
+
+#testing tags
+tag_dictionary=["html","head","body","a","div","h1","h2","h3","h4","h5","h6","li","ol","ul","p","span"]
+#testing tags
+
+tag_transformA=["<html>","<head>","<body>","<a>","<div>","<h1>","<h2>",
+"<h3>","<h4>","<h5>","<h6>","<li>","<ol>","<ul>","<p>","<span>"]
+tag_transformB=["</html>","</head>","</body>","</a>","</div>","</h1>","</h2>",
+"</h3>","</h4>","</h5>","</h6>","</li>","</ol>","</ul>","</p>","</span>"]
+
+stackA=[]
+stackB=[]
+
+count=0
+newline = ""
+
+def isolate(line):
+    linecheck = line.replace("{","")
+    newline = linecheck.strip()
+    if newline in tag_dictionary:
+        return True
+    else:
+        return False
+
+def AddtoStack():
+    for line in open:
+        if isolate(line):
+            print(line.strip() + " [Added to Stacker]")
+            stackA.append(line)
+        if not isolate(line):
+            print(line.strip() + " [Not added to Stacker]")
+
+AddtoStack()
+print("\n\nStackerA content: " + str(stackA))
