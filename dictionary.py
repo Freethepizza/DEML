5f9624e97b515a2c181aefa77b1b0b03063606ef
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-02-27 13:05:35 +0100

    Modular Shit
    improve nesting and finish with basic tags, separated stacker and dictionary with 4 Stacks.

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+#
+#    .___             .__
+#  __| _/____   _____ |  |
+# / __ |/ __ \ /     \|  |
+#/ /_/ \  ___/|  Y Y  \  |__
+#\____ |\___  >__|_|  /____/
+#     \/    \/      \/
+#
+#Dictionary
+
+
+#PRE
+Simple_dictionary = ["html","head","body","a","div","h1","h2","h3","h4",
+"h5","h6","li","ol","ul","p","span","b","i"]
+Complex_dictionary = []
+Special_dictionary = []
+
+#POST
+Stack_A = []
+Stack_B = []
+Stack_C = []
+GeneralStack = []
---
+++ stacker.py
@@ -0,0 +1,20 @@
+#
+#    .___             .__
+#  __| _/____   _____ |  |
+# / __ |/ __ \ /     \|  |
+#/ /_/ \  ___/|  Y Y  \  |__
+#\____ |\___  >__|_|  /____/
+#     \/    \/      \/
+#
+#Stacker
+
+from dictionary import Stack_A,Stack_B,Stack_C,GeneralStack
+
+def To_Stack_A(tag):
+    Stack_A.append(tag)
+def To_Stack_B(tag):
+    Stack_B.append(tag)
+def To_Stack_C(tag):
+    Stack_C.append(tag)
+def To_GeneralStack(tag):
+    GeneralStack.append(tag)
