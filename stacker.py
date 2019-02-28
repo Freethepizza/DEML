1e668797c9598416c002656538545fddec801b26
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-02-28 13:48:34 +0100

    shit

--- stacker.py
+++ stacker.py
@@ -11,6 +11,8 @@
 from dictionary import Stack_A,Stack_B,Stack_C,GeneralStack,NestingStack,ControlStack,FinalStack
 from error import *
 
+close=[]
+
 def To_Stack_A(tag):
     Stack_A.append(tag)
 def To_Stack_B(tag):
@@ -27,7 +29,9 @@ def To_FinalStack():
         for output in range(0, len(GeneralStack)):
             if "O" in ControlStack[output]:
                 FinalStack.append("<"+GeneralStack[output]+">")
+                close.append("</"+GeneralStack[output]+">")
             elif "T" in ControlStack[output]:
                 FinalStack.append(GeneralStack[output])
             elif "C" in ControlStack[output]:
-                FinalStack.append("</>")
+                FinalStack.append(close[output-output-1])
+                print()
