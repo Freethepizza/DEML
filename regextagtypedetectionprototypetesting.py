9aa96b7fd6891b57d777f4995edf98e606f3cbc7
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-04-22 13:42:24 +0200

    Tag identification and parsing
    returns simple and complex tags ready for HTML compilation, pending 0.3testing implementation

--- regextagtypedetectionprototypetesting.py
+++ regextagtypedetectionprototypetesting.py
@@ -12,77 +12,49 @@ tclass = re.compile('[a-zA-Z]+[.]+.*\{|[a-zA-Z]+[.]+.*\s\{|[a-zA-Z]+[.]+.*\n\{')
 tid = re.compile('[a-zA-Z]+[#]+.*\{|[a-zA-Z]+[#]+.*\s\{|[a-zA-Z]+[#]+.*\n\{')
 #simple tag
 st = re.compile('\w+[a-zA-Z]\{|[a-zA-Z]\s\{|[a-zA-Z]\n\{')
-'''
-word0 = "span{"
-word = "h6{"
-word2 = "h1 {"
-word3 = "h1\n{"
-xx = re.match(st,word0)
-print(xx)
-print(bool(xx))
-x = re.match(hx, word)
-print(x)
-print(bool(x))
-y = re.match(hx, word2)
-print(y)
-print(bool(y))
-z = re.match(hx, word3)
-print(z)
-print(bool(z))
 
-word4 = "div.clase,clase{"
-w = re.match(tclass, word4)
-print(w)
-print(bool(w))
+StackA = []
+StackB = []
+StackC = []
 
-word6 = "div#id,id{"+
-v = re.match(tid, word6)
-print(v)
-print(bool(v))
+def tagtype(tag):
+    classtag = re.match(tclass,tag)
+    idtag = re.match(tid, tag)
+    simpletag = re.match(st, tag)
 
-if bool(w) is True:
-    newword4 = word4.replace("{","")
-    trozos = newword4.split('.')
-    print(trozos)
-    print("<"+trozos[0]+ " class='"+trozos[1].replace(","," ") + "'>")
-else:
-    print("no match")
-'''
-def clean(what):
-    return what.replace("{\n","")
+    if bool(classtag) is True:
+        return 0
+    elif bool(idtag) is True:
+        return 1
+    elif bool(simpletag) is True:
+        return 2
 
-stackA = []
 
-#Tag diferenciation process
+def clean(tag):
+    tag.replace("{","")
+    if tagtype(tag) is 0:
+        return tag.replace("{","")
+    elif tagtype(tag) is 1:
+        return tag.replace("{","")
+    elif tagtype(tag) is 2:
+        return tag.replace("{","")
+
+
+def transform(tag):
+    if tagtype(tag) is 0:
+        newtag =  clean(tag).split(".")
+        return "<" + newtag[0] + " class='"+ newtag[1].replace("\n", "") + "'>"
+    elif tagtype(tag) is 1:
+        newtag = clean(tag).split("#")
+        return "<" + newtag[0] + " id='"+ newtag[1].replace("\n", "") + "'>"
+    elif tagtype(tag) is 2:
+        newtag = clean(tag)
+        return "<" + newtag.replace("\n", "") + ">"
+
 for lines in deml:
-    i = re.match(tclass,lines)
-    u = re.match(tid,lines)
-    e = re.match(st,lines)
+    clean(lines)
+    StackA.append(transform(lines))
+
+for lines in StackA:
     print(lines)
-    if bool(i) is True:
-        print(lines + "    TAG WITH CLASS!")
-        new = clean(lines)
-        print(new)
-        newseparated = new.split(".")
-        classes = newseparated[1].replace(","," ")
-        finalclasstag = "<" + newseparated[0] + " class='"+classes.replace("\n","")+"'>"
-        print(finalclasstag)
-        stackA.append(finalclasstag)
-    elif bool(u) is True:
-        print(lines+ "     TAG WITH ID!")
-        new = clean(lines)
-        newseparated = new.split("#")
-        ids = newseparated[1].replace(","," ")
-        finalidtag =  "<" + newseparated[0] + " id='"+ids.replace("\n","")+"'>"
-        stackA.append(finalidtag)
-    elif bool(e) is True:
-        print(lines + "    SIMPLE TAG!")
-        new = clean(lines)
-        finaltag = "<"+ new +">"
-        stackA.append(finaltag)
-
-
-print(stackA)
-
-for elements in stackA:
-    print(elements + "\n")
+print(StackA)
